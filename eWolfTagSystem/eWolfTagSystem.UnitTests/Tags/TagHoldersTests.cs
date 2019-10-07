aeaede8a320b03462c8353722d0dddbc46ab33c9
ElectricWolfGames <39939684+ElectricWolfGames@users.noreply.github.com>
2019-10-07 17:43:13 +0100

    First commit, start of the image tagger

---
+++ eWolfTagSystem/eWolfTagSystem.UnitTests/Tags/TagHoldersTests.cs
@@ -0,0 +1,32 @@
+﻿using eWolfTagSystem.Tags;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace eWolfTagSystem.UnitTests.Tags
+{
+    public class TagHoldersTests
+    {
+        [Test]
+        public void ShouldCreateTagHolders()
+        {
+            TagHolders th = new TagHolders("Test Tags");
+            th.Line.Should().Be("Test Tags");
+        }
+
+        [Test]
+        public void ShouldUpdateTagHolders()
+        {
+            TagHolders th = new TagHolders("Test Tags");
+            th.Line = "Tag2 Tag";
+            th.Line.Should().Be("Tag2 Tag");
+        }
+
+        [Test]
+        public void ShouldAddTagsToTagHolders()
+        {
+            TagHolders th = new TagHolders("Test Tags");
+            th.AddTag("Another tag");
+            th.Line.Should().Be("Test Tags AnotherTag");
+        }
+    }
+}
---
+++ eWolfTagSystem/eWolfTagSystem/Helpers/TagHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace eWolfTagSystem.Helpers
+{
+    public static class TagHelper
+    {
+        public static string[] GetTagsFromName(string name)
+        {
+            string[] parts = name.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            List<string> trimmedParts = new List<string>();
+
+            foreach (string part in parts)
+            {
+                trimmedParts.Add(part.Trim());
+            }
+
+            return trimmedParts.ToArray();
+        }
+
+        public static string CreateFileNameFromTags(string[] parts)
+        {
+            List<string> words = new List<string>();
+            foreach (string part in parts)
+            {
+                words.Add(MakePascalCase(part));
+            }
+
+            return string.Join(" ", words);
+        }
+
+        public static string MakePascalCase(string line)
+        {
+            string clearnLine = line.Replace("'", string.Empty);
+            string[] words = clearnLine.Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            StringBuilder sb = new StringBuilder();
+            foreach (string word in words)
+            {
+                string wordHolder = word.First().ToString().ToUpper() + word.Substring(1);
+                sb.Append(wordHolder);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
