169860d0b5d0a7fecdd90f34d390a6e199d992b2
Karl Gillott <karlgillott@GoogleMail.com>
2021-02-11 06:44:58 +0000

    Added new core version of the tagger system and separate VIEW and core code

---
+++ eWolfMetaTaggerCommon.UnitTests/TagData/BasicTagListBaseTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eWolfMetaTaggerCommon.UnitTests.TagData
+{
+    class BasicTagListBaseTests
+    {
+    }
+}
---
+++ eWolfMetaTaggerCommon/TagData/BasicTagListBase.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace eWolfMetaTaggerCommon.TagData
+{
+    public class BasicTagListBase
+    {
+    }
+}
